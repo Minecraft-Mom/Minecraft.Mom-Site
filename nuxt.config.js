b45af8f717f3a755b1ed90347d5d83aa676f3fc1
Ethan <ethan@spangler.dev>
2021-09-26 15:51:31 -0400

    Update nuxt.config.js
    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mcmom',
+    title: 'minecraft.mom',
     htmlAttrs: {
       lang: 'en'
     },
