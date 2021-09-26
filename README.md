4c1baf1d9d54f9587b7bf9ba485158e3e9f53475
Ethan <ethan@spangler.dev>
2021-09-26 15:44:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft.Mom
+
+This is a simple Nuxt.js site for the domain minecraft.mom
---
+++ components/Mom.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-screen bg-gradient-to-tr from-red-400 to-purple-900">
+    <div class="m-auto">
+      <img class="h-32" src="~assets/img/MinecraftMomIconLarge.svg" />
+    </div>
+  </div>
+</template>
